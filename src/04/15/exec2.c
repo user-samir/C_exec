775f401ad2ab9927fce1d0f89b5f5e6a356cff8e
samir-muhammadov-mrb8953 <mukhammadov.sam@yandex.ru>
2024-11-19 21:11:02 +0300

    exec2

--- src/04/15/exec2.c
+++ src/04/15/exec2.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void printFactor(unsigned int last, unsigned int n);
+void simpleFactors(unsigned int a, unsigned int last, unsigned int n);
+
 int main()
 {
     unsigned int a;
@@ -7,4 +10,31 @@ int main()
     simpleFactors(a, 2, 0);
 
     return 0;
+}
+
+void printFactor(unsigned int last, unsigned int n)
+{
+    if (n == 0)
+        return;
+    if (n == 1)
+        printf("%u ", last);
+    else
+        printf("%u^%u ", last, n);
+}
+
+void simpleFactors(unsigned int a, unsigned int last, unsigned int n)
+{
+    if (a == 1)
+    {
+        printFactor(last, n);
+        return;
+    }
+
+    if (a % last == 0)
+        simpleFactors(a/last, last, n+1);
+    else
+    {
+        simpleFactors(a, last+1, 0);
+        printFactor(last, n);
+    }
 }
